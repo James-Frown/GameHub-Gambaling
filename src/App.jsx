26a1fdef902087f459cda703cc3dcb16c0c2b9d2
James-Frown <21122@ctca.co.za>
2023-05-21 18:32:10 +0200

    Latest Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-
-import { home } from './pages/home/home';
-import { login } from './pages/login/login';
-
 import './App.css';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" component={login} />
-        <Route path="/home" component={home} />
+        <Route path="/" component={} />
+        <Route path="/login" component={} />
+        <Route path="/signup" component={} />
       </Routes>
     </BrowserRouter>
   )
