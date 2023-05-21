3b84a105bc6ca5d2a368854dca94f8e98eb0b761
James-Frown <21122@ctca.co.za>
2023-05-21 18:29:18 +0200

    Latest Changes

---
+++ src/pages/signup/signup.jsx
@@ -0,0 +1,45 @@
+// import fire from '../../config/fire';
+
+// import './login.css';
+
+const signup = () => {
+
+    function signupHandler() {
+        console.log("signup clicked");
+    }
+
+    return (
+        <>
+            <div>
+                <div>
+                    <h1>
+                        Login Page
+                    </h1>
+                    <p>
+                        Enter Your Details...
+                    </p>
+                </div>
+
+                <div>
+                    <form id="login-form">
+                        <label>
+                            Please enter your Email:
+                            <input id="email" type='text' placeholder='eg: name@gmail.com'>
+                            </input>
+                        </label>
+                        <label>
+                            Please enter a Password:
+                            <input id="password" type='text' placeholder='eg: 123456789'>
+                            </input>
+                        </label>
+                        <div>
+                            <button onClick={signupHandler}>SignUp</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </>
+    )
+};
+
+export default signup;
